861c290f9d2ac6f4f8dab7298832d3799a3ca103
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-01-23 12:20:03 -0800

    Add files via upload

--- pythonclubproj/club/admin.py
+++ pythonclubproj/club/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Meeting,Minutes,Event,Resource 
 # Register your models here.
+
+admin.site.register(Meeting)
+admin.site.register(Resource)
+admin.site.register(Event)
+admin.site.register(Minutes)
--- pythonclubproj/club/models.py
+++ pythonclubproj/club/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Meeting(models.Model):
+    meetingtitle=models.CharField(max_length=255)
+    meetingdate=models.DateField()
+    meetingtime=models.TimeField()
+    meetinglocation=models.CharField(max_length=255)
+    meetingagenda=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table='meeting'
+
+class Minutes(models.Model):
+    minutestitle=models.CharField(max_length=255)
+    meeting=models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    meetingattendance=models.ManyToManyField(User)
+    minutestext=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.minutestitle
+
+    class Meta:
+        db_table='minutes'
+
+class Resource(models.Model):
+    resourcename=models.CharField(max_length=255)
+    resourcetype=models.CharField(max_length=255)
+    resourceurl=models.URLField(null=True, blank=True)
+    resourcedate=models.DateField()
+    resourcedateentered=models.DateField()
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    resourcedescription=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.recourcename
+
+    class Meta:
+        db_table='resource'
+
+class Event(models.Model):
+    eventtitle=models.CharField(max_length=255)
+    eventlocation=models.CharField(max_length=255)
+    eventdate=models.DateField()
+    eventtime=models.TimeField()
+    eventdescription=models.CharField(max_length=255)
+    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.eventtitle
+
+    class Meta:
+        db_table='event'    
