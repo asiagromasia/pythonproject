e74a0e0adfc73bead470f3603f3b97d0d0c5e97b
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-02-06 12:49:06 -0800

    upload new changes

--- pythonclubproj/club/urls.py
+++ pythonclubproj/club/urls.py
@@ -6,5 +6,15 @@ from . import views
 urlpatterns=[
     path('', views.index, name='index'),
     # index is different than the rest so now you have to do the name of the view
-    path('resources/', views.resources, name='resources')
+    path('resources/', views.resources, name='resources'),
+    path('meetings', views.meetings, name='getmeetings' ),
+    path('minutes/<int:id>', views.minutes, name='minutes'),
 ]
+
+# urlpatterns=[
+#     path('', views.index, name='index'),
+#     # index is different than the rest so now you have to do the name of the view
+#     path('techtypes/', views.techtypes, name='techtypes'),
+#     path('getproducts',views.getproducts, name='getproducts'),
+#     path('productdetail/<int:id>', views.productdetail, name='details'),
+# ]
--- pythonclubproj/club/views.py
+++ pythonclubproj/club/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Resource 
+from django.shortcuts import render, get_object_or_404
+from .models import Resource, Meeting, Minutes
 
 # Create your views here.
 def index (request):
@@ -10,3 +10,13 @@ def resources (request):
     resource_list=Resource.objects.all()
     # if you have a big table than you can select only some
     return render (request, 'club/resources.html',{'resource_list': resource_list})
+
+def meetings (request):
+    meeting_list=Meeting.object.all()
+    return render (request, 'club/meetings.html', {'meeting_list': meeting_list})
+
+def minutes (request, id):
+    minutes=get_object_or_404(Meeting, pk=id)
+    # minutes_list=Meeting.objects.all()
+    return render (request, 'club/minutes.html', {'minutes_list':minutes})    
+
